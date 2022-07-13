3d35bf12136bd5d88b9e89c9acba075217a3f4b1
Sinojgeorge <96327967+Sinojgeorge@users.noreply.github.com>
2022-07-13 19:12:54 +0530

    Add files via upload

--- Area.java
+++ Area.java
@@ -1,24 +1,26 @@
-import java.io.*;
-import java.util.*;
-import java.util.Scanner;
-class Area{
-    void squareArea(float x)
-    {
-        System.out.println("Area of square="+(x*x)+"units");
-    }
-    void rectArea(float x,float y)
-    {
-        System.out.println("Area of rectangle is"+(x*y)+"units");
-    }
-    void circleArea(double r)
-    {
-        double a=3.14*r*r;
-        System.out.println("area of circle="+a+"units");
-    }
-    public static void main(String[] args){
-        Area obj=new Area();
-        obj.squareArea(5);
-        obj.rectArea(5,6);
-        obj.circleArea(7);
-    }
+class Shapes
+{
+	int area(int a)
+	{
+		return a*a;
+	}
+	int area(int a,int b)
+	{
+		return a*b;
+	}
+	 double area(double r)
+	{
+	    return 3.14*r*r;
+	}
+}
+
+public class Area
+{
+	public static void main(String args[])
+	{
+		Shapes sh= new Shapes();
+		System.out.println(sh.area(5));
+		System.out.println(sh.area(5,4));
+		System.out.println(sh.area(2.00));
+	}
 }
